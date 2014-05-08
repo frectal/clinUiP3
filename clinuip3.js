89c7aa395fe23e2aae705efc4038ce226d27bb21
frectal <frectal@users.noreply.github.com>
2014-05-08 15:05:34 +0100

    Tidy up
    Tided up both files to make easier to read

--- clinuip3.js
+++ clinuip3.js
@@ -1,4 +1,7 @@
 // set up vars to use for dev purposes
+
+// these json docs are the basis for the data .. get stored in the M database and then the JSON document grows in the M database
+// ie you will see the start of the JSON documents here but the rest of it is stored in the M database
 var PatientStatements = {
   "PatientStatements": [
     {
@@ -20,170 +23,23 @@ var PatientStatements = {
 } ; 
 
 
-var DeptStatus = {    // NOTE that this has been changed from an object to an array in the client side version-- needs to be changed too
-    "DepartmentStatus": {
-        "ER": {
-            "Name": "Emergency Department",
-            "Sites": "2 EDs: 1 Trauma; 1 Medical",
-            "Males": 134,
-            "Females": 100,
-            "Total": 234
-        },
-        "IP": {
-            "Name": "In Patients",
-            "Sites": "12 wards",
-            "Males": 96,
-            "Female": 49,
-            "Total": 145
-        },
-        "OP": {
-            "Name": "Out Patients",
-            "Sites": "15 clinics",
-            "Males": 332,
-            "Female": 447,
-            "Total": 779
-        }
-    }
-};
-
 var testPatients = {
   "Patients": [
     {
       "ID": 1233555,
       "Sex": "Male",
       "Age": 58,
-      "Name": "John Mick Dobson",
-      "Encounters": [
-        {
-          "Date Open": "2013 June 11th",
-          "Type": "Inpatient Stay",
-          "Date Closed": "2013 June 15th"
-        },
-        {
-          "Date Open": "2014 June 11th",
-          "Type": "Outpatient Visit",
-          "Date Closed": ""
-        }
-      ],
-      "Allergies": [
-        {
-          "Causative Agent": "Latex",
-          "Date First Experienced": "2013 June 11th",
-          "Descriptive Reaction": "Local Rash",
-          "Probability of Recurrence": "High"
-        },
-        {
-          "Causative Agent": "Gluten",
-          "Date First Experienced": "2013 June 11th",
-          "Descriptive Reaction": "Bloating",
-          "Probability of Recurrence": "High"
-        }
-      ],
-      "InvestigationsResults": [
-        {
-          "OrderID": 23456,
-          "Investigation": "FBC- HB count",
-          "Date": "2013 March 17",
-          "Result": "13.5 mg/L"
-        },
-        {
-          "OrderID": 23456,
-          "Investigation": "U&E- K level",
-          "Date": "2013 March 17",
-          "Result": "5.5 mg/L"
-        }
-      ],
-      "Diagnoses": [
-        "Hypertension",
-        "Angina"
-      ],
-      "Treatments": [
-        {
-          "DrugID": 23456,
-          "Name": "Furosemide",
-          "Dose": "20mg",
-          "Frequency": "BD"
-        },
-        {
-          "DrugID": 23877,
-          "Name": "GTN spray",
-          "Dose": "2 puffs",
-          "Frequency": "PRN"
-        }
-      ],
-      "Procedures": [
-        {
-          "Procedure": "Total Hip Replacement (R)",
-          "Date of Procedure": "2011 June 11th"
-        },
-        {
-          "Procedure": "Total Knee Replacement (R)",
-          "Date of Procedure": "2012 June 11th"
-        }
-      ],
-      "PlansActions": [
-        {
-          "Plan": "Home Visit to check Mobility",
-          "Date for Action": "2011 June 11th",
-          "Actioned Note": "Done- Good Mobility"
-        },
-        {
-          "Plan": "Orthopaedic Checkup",
-          "Date for Action": "2014 June 11th",
-          "Actioned Note": ""
-        }
-      ]
+      "Name": "John Micky Dobson"
+	  
     },
     {
       "ID": 1233444,
       "Sex": "Female",
       "Age": 54,
-      "Name": "Jane M Dilly",
-      "Diagnoses": [
-        "Diverticulitis",
-        "Appendicitis"
-      ],
-      "Treatments": [
-        "Paracetamol",
-        "Ibuprofen"
-      ]
+      "Name": "Jane M Dilly"
+	  
     }
   ]
-};
-
-
-
-var testPatients2 = {
-    "Patients": [
-        {
-            "ID": 1233555,
-            "Sex": "Male",
-            "Age": 58,
-            "Name": "John DobsonMMM",
-            "Diagnoses": [
-                "Hypertension",
-                "Angina"
-            ],
-            "Treatments": [
-                "Furosemide",
-                "GTN Spray"
-            ]
-        },
-        {
-            "ID": 1233444,
-            "Sex": "Female",
-            "Age": 54,
-            "Name": "Jane DillyM",
-            "Diagnoses": [
-                "Diverticulitis",
-                "Appendicitis"
-            ],
-            "Treatments": [
-                "Paracetamol",
-                "Ibuprofen"
-            ]
-        }
-    ]
 };
 
 
@@ -193,7 +49,8 @@ module.exports = {
 
 
 
-
+// this is how to interact with the EWD and hence the M database
+// via a series of web socket calls
 
 onSocketMessage: function(ewd) {
 var wsMsg = ewd.webSocketMessage;
@@ -201,6 +58,8 @@ var type = wsMsg.type;
 var params = wsMsg.params;
 var sessid = ewd.session.$('ewd_sessid')._value;
 
+
+// this is just a test message
 if (type === 'sendHelloWorldX') { 
 ewd.log('*** Incoming Web Socket message received: ' + JSON.stringify(params, null, 2), 1);
 var savedMsg = new ewd.mumps.GlobalNode('%AMessage', []);
@@ -208,157 +67,155 @@ savedMsg._setDocument(wsMsg);
 return {savedInto: '%AMessage'};
 }
 
-//if (type === 'getHelloWorldY') {
+
+// this is the start of the useful EWD calls
+// this one calls the patient list
+// it creates a node on the M database and then stores the JSON document in there
 if (type === 'getPatientList') {
-//ClinUiP3
-//var HospitalZ = new ewd.mumps.GlobalNode('HospitalZ', ['0001','Patients']);
+
 var HospitalZ = new ewd.mumps.GlobalNode('HospitalZ', ['0001']);
 console.log("Setting up new M variable");
 // note that you need a name for a global with '' .. a number.. 
 // must have ewd.mumps.GlobalNode
 // with [ and ' around it and end the number with ' and ]
-//testHospital3._setDocument(PtNumbers);
 console.log("trying to set JSON to M");
 
-//#########this is where we set the json to the M database
-//HospitalZ._setDocument(DeptStatus);
+//#########this is where we set the json to the M database with setDocument
 HospitalZ._setDocument(testPatients);
-
-//HospitalZ._setDocument(PatientStatements);
 // #### this is where we end setting that json to M database 
-// show that we can get JSON out of the M DB
 
+// now show that we can get JSON out of the M DB with getDocument
 var Houtput = HospitalZ.Patients._getDocument();
-//var Houtput = HospitalZ._getDocument();
-console.log("JSON output from M to follow...");
 var ListPatients = "{" + Houtput  + "}"; // set up new object
-
-//ListPatients = + testPatients;
+//series of console logs to compare outputs
+console.log("JSON output from M to follow...");
 console.log("Patients:" + JSON.stringify(Houtput, null, 4));
 console.log(JSON.stringify(testPatients, null, 4));
 console.log(JSON.stringify(ListPatients, null, 4));
 
 
-//helloworld
-var savedMsg = new ewd.mumps.GlobalNode('%AMessage', []);
-//return savedMsg._getDocument();
-//return ListPatients;
 return Houtput;
 }
 
 
+// have decided to use server side JS to do calls for Male & Female patients
+// am sure there are better ways to do this
 
 if (type === 'getPatientListFemales') {
 var HospitalZ = new ewd.mumps.GlobalNode('HospitalZ', ['0001']);
-console.log("Setting up new M variable");
 console.log("trying to set JSON to M");
 
-HospitalZ._setDocument(testPatients);
-//HospitalZ._setDocument(PatientStatements);
 // #### this is where we end setting that json to M database 
+HospitalZ._setDocument(testPatients);
 // show that we can get JSON out of the M DB
 var Houtput = HospitalZ.Patients._getDocument();
+
 return Houtput;
 }
 
 
 if (type === 'getPatientListMales') {
 var HospitalZ = new ewd.mumps.GlobalNode('HospitalZ', ['0001']);
-console.log("Setting up new M variable");
 console.log("trying to set JSON to M");
 
+// #### this is where we end setting that json to M database
 HospitalZ._setDocument(testPatients);
-//HospitalZ._setDocument(PatientStatements);
-// #### this is where we end setting that json to M database 
 // show that we can get JSON out of the M DB
 var Houtput = HospitalZ.Patients._getDocument();
+
 return Houtput;
 }
 
+
+// this is where we get the Patient Statements out
 if (type === 'getPatientStatements') {
-//ClinUiP3
-//var HospitalZ = new ewd.mumps.GlobalNode('HospitalZ', ['0001','Patients']);
 var HospitalZ = new ewd.mumps.GlobalNode('HospitalZ', ['0001']);
 //console.log("Setting up new M variable");
-// note that you need a name for a global with '' .. a number.. 
-// must have ewd.mumps.GlobalNode
-// with [ and ' around it and end the number with ' and ]
-//testHospital3._setDocument(PtNumbers);
 //console.log("trying to set JSON to M");
 
 //#########this is where we set the json to the M database
-//HospitalZ._setDocument(DeptStatus);
-//HospitalZ._setDocument(testPatients);
-
 HospitalZ._setDocument(PatientStatements);
 // #### this is where we end setting that json to M database 
+
 // show that we can get JSON out of the M DB
-
 var PSoutput = HospitalZ.PatientStatements._getDocument();
-//var Houtput = HospitalZ._getDocument();
-//console.log("JSON output from M to follow...");
-//var ListPatients = "{" + Houtput  + "}"; // set up new object
 
-//ListPatients = + testPatients;
 console.log("PatientStatements:" + JSON.stringify(PSoutput, null, 4));
-//console.log(JSON.stringify(testPatients, null, 4));
-//console.log(JSON.stringify(ListPatients, null, 4));
 
-
-//helloworld
-//var savedMsg = new ewd.mumps.GlobalNode('%AMessage', []);
-//return savedMsg._getDocument();
-//return ListPatients;
 return PSoutput;
 }
 
 
 
-
+// this is where we add Patients to the M/ JSON database
 
 if (type === 'add2PatientM') { 
+// 2 forms for console logging.. in the EWD monitor and the regular node console
 ewd.log('**Incoming PatientMonEWD message: ' + JSON.stringify(params, null, 2), 1);
 console.log('**Incoming PatientMMM message: ' + JSON.stringify(params, null, 2), 1);
 
-var HospitalZ = new ewd.mumps.GlobalNode('HospitalZ', ['0001','Patient']);
-
+// get the current JSON doc of Patients
+var HospitalZ = new ewd.mumps.GlobalNode('HospitalZ', ['0001','Patients']);
 var Hinput = HospitalZ._getDocument();
+
+// we take the parameters that are being passed in this call
+// and "push" it into the JSON
 var patientY = params; 
 Hinput.push(patientY);
-
+// then set the JSON document again
 HospitalZ._setDocument(Hinput);
-//testPatients.Patients.push(patientY);
+
+// then a large console log so we can see it
 console.log(JSON.stringify(Hinput, null, 4));
 console.log("+++++++€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€+++++++++++++++++++++++++++++++");
-//var savedMsg = new ewd.mumps.GlobalNode('%AMessage', []);
-//savedMsg._setDocument(wsMsg);
-//return {savedInto: '%AMessage'};
+
 return true;
 }
 
 
-
+// this is where we add Patients to the M /JSON database
 if (type === 'addStatements2PatientEHR') { 
+// 2 forms for console logging.. in the EWD monitor and the regular node console
+
 ewd.log('**Incoming StatementEWD message: ' + JSON.stringify(params, null, 2), 1);
 console.log('**Incoming StatementM message: ' + JSON.stringify(params, null, 2), 1);
 
+// get the current JSON doc of Patient statements
 var HospitalZ = new ewd.mumps.GlobalNode('HospitalZ', ['0001','PatientStatements']);
-
 var Sinput = HospitalZ._getDocument();
+
+// take the parameters being sent
 var ptstatements = params; 
+// and "push" them into the JSON array
 Sinput.push(ptstatements);
 
+// then set an update version of the JSON doc in the M database
 HospitalZ._setDocument(Sinput);
-//testPatients.Patients.push(patientY);
+
+
+// then a large console log so we can see it
 console.log(JSON.stringify(Hinput, null, 4));
 console.log("+++++++&&&&&&&&&&&&&&&+++++++++++++++++++++++++++++++");
-//var savedMsg = new ewd.mumps.GlobalNode('%AMessage', []);
-//savedMsg._setDocument(wsMsg);
-//return {savedInto: '%AMessage'};
+
 return true;
 }
 
 
-//ewd.log('*** Incoming Web Socket message @@@received: ' + JSON.stringify(wsMsg, null, 2), 1);
+// Exploring the JSON on the M database
+// if you want to explore the M database//
+// go to linux command line/
+// type mumps -dir
+// then 
+// zwr ^HospitalZ  
+// zwr ^HospitalZ("0001","Patients",2,*)
+// etc etc
+// to explore the data
+
+
+// Note re M /JSON database issue
+// if poor data quality.. then need to "kill" elements of the global.. 
+// but if you take number 5 of of 1,2,3,4,5,6,7.. then that messes up the return and the table looks empty
+// so we currently needs to kill 5,6,7 to cut the array to 1,2,3,4
+ 
 } // end of onSocketMsg
 }; // end of modules
